0b92fa085a0c2ed5c35c41a8e5ae78a81c4be438
Amany <amanyadel20242024@gmail.com>
2025-03-23 07:04:17 +0200

    homeviewmodel

--- app/src/main/java/com/amany/taks/home/HomeViewModel.kt
+++ app/src/main/java/com/amany/taks/home/HomeViewModel.kt
@@ -1,4 +1,39 @@
 package com.amany.taks.home
 
-class HomeViewModel {
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.amany.taks.remote.WeatherState
+import com.amany.taks.repository.WeatherRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+
+class HomeViewModel(private val weatherRepository: WeatherRepository):ViewModel(){
+    private val _currentWeather: MutableStateFlow<WeatherState> = MutableStateFlow<WeatherState>(WeatherState.Loading)
+    val currentWeather : StateFlow<WeatherState> = _currentWeather
+
+    fun getCurrentWeather(latitude: Double, longitude: Double  , units: String , lang:String) {
+        viewModelScope.launch {
+            weatherRepository.getCurrentWeather(latitude, longitude, units ,lang)
+                .catch {
+                    _currentWeather.value = WeatherState.Failure(it)
+                }
+                .collect{
+                        data ->
+                    _currentWeather.value = WeatherState.Success(data)
+                }
+        }
+    }
+
+
 }
+class HomeScreenViewModelFactory (private val _repo: WeatherRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
+        return HomeViewModel(_repo) as T
+
+    }}
