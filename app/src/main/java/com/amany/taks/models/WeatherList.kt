713510c220383b0fedbf926d3bba35bf021c12d3
Amany <amanyadel20242024@gmail.com>
2025-03-28 15:48:53 +0200

    Days forcast

--- app/src/main/java/com/amany/taks/models/WeatherList.kt
+++ app/src/main/java/com/amany/taks/models/WeatherList.kt
@@ -21,6 +21,15 @@ data class WeatherList(
 ) {
 
 }
+data class ForecastResponse(
+    val list: List<ForecastItem>
+)
+
+data class ForecastItem(
+    val dt: Long,
+    val main: Main,
+    val weather: List<Weather>
+)
 data class AlarmData(
     val id: Int,
     var time: Long
